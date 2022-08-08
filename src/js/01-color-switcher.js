cf85d1c6aca5e373664a4638aa8bf1bb210f538f
Martovytskyi Pavlo <viy.1979@gmail.com>
2022-08-08 12:26:30 +0300

    start

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,10 @@
+const btStart = document.querySelector("button[data-start]");
+const btStop = document.querySelector("button[data-stop]");
 
+
+
+
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
