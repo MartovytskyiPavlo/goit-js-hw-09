39a481c0afa9c9ac1ce13fefdfd8d19ba70ffab3
Martovytskyi Pavlo <viy.1979@gmail.com>
2022-08-09 13:42:14 +0300

    start

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,12 +1,18 @@
 import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
 
+const btStart = document.querySelector("button[data-start]");
+
 const options = {
   enableTime: true,
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
-  onClose(selectedDates) {
+    onClose(selectedDates) {
+        if (selectedDates[0]<Date()) {
+            btStart.setAttribute('disabled', '');
+            window.alert("Please choose a date in the future");
+        }
     console.log(selectedDates[0]);
   },
 };
