416998d0aba9f219b21c4702eca225b4cfba4c57
Martovytskyi Pavlo <viy.1979@gmail.com>
2022-08-09 12:59:14 +0300

    start

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,9 +1,25 @@
 const btStart = document.querySelector("button[data-start]");
 const btStop = document.querySelector("button[data-stop]");
 
+let timerId;
+
+btStart.addEventListener('click', onStartClick);
+btStop.addEventListener('click', onStopClick);
 
 
+function onStopClick() {
+  btStart.removeAttribute('disabled');
+  clearInterval(timerId);
+}
 
+function onStartClick() { 
+  btStart.setAttribute('disabled', '');
+  timerId = setInterval(changeBackground , 1000);
+}
+
+function changeBackground() {
+   document.body.style.background = getRandomHexColor();
+}
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
