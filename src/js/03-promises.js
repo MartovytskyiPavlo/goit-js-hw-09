37a4d9b193629ad9b4b5e566d0684186ac8ef2a5
Martovytskyi Pavlo <viy.1979@gmail.com>
2022-08-09 17:41:44 +0300

    start

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,16 @@
+import Notiflix from 'notiflix';
+
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
     // Fulfill
+    Notiflix.Notify.success('Sol lucet omnibus');
   } else {
     // Reject
+    Notiflix.Notify.failure('Qui timide rogat docet negare');
   }
 }
+
+
+
